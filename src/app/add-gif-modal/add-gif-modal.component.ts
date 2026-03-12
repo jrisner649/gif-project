46743dc3738a3101dbb9c88983f644d0b0e8f15e
jrisner649 <jrisner649@gmail.com>
2026-03-11 20:40:31 -0500

    feat: add gif modal prototype

--- src/app/add-gif-modal/add-gif-modal.component.ts
+++ src/app/add-gif-modal/add-gif-modal.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialogContent, MatDialogActions, MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-gif-modal',
-  imports: [],
+  imports: [MatDialogContent, MatDialogActions],
   templateUrl: './add-gif-modal.component.html',
   styleUrl: './add-gif-modal.component.css',
 })
-export class AddGifModalComponent {}
+export class AddGifModalComponent {
+  dialog = inject<MatDialog>(MatDialog);
+
+  onClose() {
+    this.dialog.closeAll();
+  }
+}
