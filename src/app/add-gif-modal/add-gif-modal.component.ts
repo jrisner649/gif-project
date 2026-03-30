1313af30282274066c7de773702489cdf1f1446d
jrisner649 <jrisner649@gmail.com>
2026-03-29 19:33:29 -0500

    chore(deps): install scss migrate and material

--- src/app/add-gif-modal/add-gif-modal.component.ts
+++ src/app/add-gif-modal/add-gif-modal.component.ts
@@ -1,14 +1,15 @@
 import { AfterViewInit, Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatDialog, MatDialogContent, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-add-gif-modal',
-  imports: [FormsModule],
+  imports: [FormsModule, MatDialogContent, MatDialogActions, MatDialogClose],
   templateUrl: './add-gif-modal.component.html',
   styleUrl: './add-gif-modal.component.css',
 })
 export class AddGifModalComponent {
-  @ViewChild('addGIFModal') modalRef!: ElementRef;
-
-  linkInputValue: string = '';
+  
+ 
 }
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AddGifModalComponent } from '../add-gif-modal/add-gif-modal.component';
+import * as bootstrap from 'bootstrap';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-top-bar',
-  imports: [],
+  imports: [AddGifModalComponent],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css',
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+  dialog = inject(MatDialog);
+  
+   onAddGIF() {
+    this.dialog.open(AddGifModalComponent);
+  }
+}
