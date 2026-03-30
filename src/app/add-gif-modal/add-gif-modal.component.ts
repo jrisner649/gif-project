a1bf0ba03ea0f599dcc932d908ac9f01b395e19c
jrisner649 <jrisner649@gmail.com>
2026-03-29 20:40:49 -0500

    fix: match outline to modal border

--- src/app/add-gif-modal/add-gif-modal.component.ts
+++ src/app/add-gif-modal/add-gif-modal.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import {
   MatDialog,
   MatDialogContent,
@@ -9,7 +10,7 @@ import {
 
 @Component({
   selector: 'app-add-gif-modal',
-  imports: [FormsModule, MatDialogContent, MatDialogActions, MatDialogClose],
+  imports: [FormsModule, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
   templateUrl: './add-gif-modal.component.html',
   styleUrl: './add-gif-modal.component.scss',
 })
