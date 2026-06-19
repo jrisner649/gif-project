819f32d645d804d6eae33c3bbecf0f0ca35dab7f
jrisner649 <jrisner649@gmail.com>
2026-06-19 13:24:53 -0500

    add placeholder for new dashboard gif

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  link = 'something'
+}
