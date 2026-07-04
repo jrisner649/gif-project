6b9bd6626683cb132ec4d24514325fcf4f4f61cc
jrisner649 <jrisner649@gmail.com>
2026-07-04 11:40:20 -0500

    move sidebar to app component to encompass both top bar and dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent, FormsModule, DashboardComponent],
+  imports: [RouterOutlet, TopBarComponent, FormsModule, DashboardComponent, MatSidenavModule, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { AddGifService } from '../add-gif-service/add-gif.service';
-import {
-  MatSidenavModule
- } from '@angular/material/sidenav';
-import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatSidenavModule, SidebarComponent],
+  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
