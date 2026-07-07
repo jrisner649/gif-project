e1097bdd32c005322fb0a0b4c6fe64cbd6800162
jrisner649 <jrisner649@gmail.com>
2026-07-06 20:07:00 -0500

    create gif component to make deletion simple

---
+++ src/app/gif/gif.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif',
+  imports: [],
+  templateUrl: './gif.component.html',
+  styleUrl: './gif.component.scss',
+})
+export class GifComponent {}
